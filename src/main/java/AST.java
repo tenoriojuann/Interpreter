cd1a17debae0fda88df5502168401c68a354e3c7
Juan <tenoriojuann@gmail.com>
2017-02-13 13:48:15 -0500

    Defined entry point for the AST

---
+++ src/main/java/AST.java
@@ -0,0 +1,26 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+/**
+ * Created by I859979 on 2/13/2017.
+ */
+public class AST {
+
+
+
+
+    JsonObject program = new JsonObject();
+
+
+    // Sets the initial properties that every program will have
+    public AST(){
+        program.addProperty("type", "program");
+        program.add("program", new JsonArray());
+    }
+
+
+    // Adds new entries to the program, per line
+    public void setObject(JsonObject object){
+        program.getAsJsonArray("program").add(object);
+    }
+}
--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,5 +1,21 @@
-/**
- * Created by I859979 on 2/13/2017.
- */
+import com.google.gson.JsonObject;
+
 public class Parser {
+
+
+
+    public Parser(){
+
+
+    }
+
+    // This will work for the int case
+    // But we'll need to re-write it for more complex things
+    public JsonObject varType(String type, int value){
+        JsonObject typeJ = new JsonObject();
+        typeJ.addProperty(type, value);
+
+        return typeJ;
+    }
+
 }

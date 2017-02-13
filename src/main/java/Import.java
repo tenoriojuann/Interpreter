32d0265e5dc2229b09e8ce1a96ac0879e2730d88
Juan <tenoriojuann@gmail.com>
2017-02-13 12:29:05 -0500

    wrote the Import Class.

--- src/main/java/Import.java
+++ src/main/java/Import.java
@@ -1,5 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 /**
  * Created by I859979 on 2/13/2017.
  */
 public class Import {
+
+    String src;
+    Import(String url) throws IOException {
+
+        newFile(url);
+
+    }
+
+    public void newFile(String url) throws IOException {
+        src =  new String(Files.readAllBytes(Paths.get(url)), "UTF-8");
+    }
+
 }

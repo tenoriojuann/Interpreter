452a90684a35c606ae09ec2c2191b3c76598ffd8
JUAN E TENORIO ARZOLA <tenoriojuann@gmail.com>
2017-02-15 18:21:55 -0500

    added method to get type and get operators from the imported line

--- src/main/java/Analyzer.java
+++ src/main/java/Analyzer.java
@@ -8,13 +8,15 @@ public class Analyzer {
     FileInputStream url;
     BufferedReader sb;
 
+    String[] splitLine;
+
+    String[] OPERATORS = {"*","-","+","/","="};
+
+    String[] TYPES = {"int", "integer"};
+
     public Analyzer(){
 
 
-    }
-
-    public enum TYPES {
-        integer, INT;
     }
 
     public void openFile(FileInputStream url){
@@ -31,8 +33,28 @@ public class Analyzer {
         }
     }
 
-    public String[] SplitLine(String line){
-        return line.split(" ");
+    public void SplitLine(String line) {
+        splitLine = line.split(" ");
+    }
+
+    public String getType(){
+
+        for(String type : TYPES) {
+            if (type.equalsIgnoreCase(splitLine[0])){
+                return type;
+            }
+        }
+        return "var";
+    }
+
+    public String getOperator(){
+
+        for(String op : OPERATORS){
+            if(splitLine[1].equals(op)){
+                return op;
+            }
+        }
+        return "none";
     }
 
 }

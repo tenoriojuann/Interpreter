4771f780b32b3c00e1b8e58d074043ad0cbe3299
majorshatz <theshatz@gmail.com>
2017-03-27 20:10:43 -0400

    Update Parser.java

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,11 +1,19 @@
 import com.google.gson.JsonObject;
-
+import java.util.Stack;
+import java.util.ArrayList;
 public class Parser {
 
 
 
     public Parser(){
-
+    //create what is going to be needed:
+    //Stack will check to see if execption is thrown.
+        Stack<String> exceptionCheck = new Stack<String>();
+    //Array list that will take in tokens. Array list makes it easy to recover if there is an exception thrown.
+    //Vector will be used if we make it in C++. 
+        ArrayList<String> tokens= new ArrayList<>();
+        
+   //Method that uses Scanner to take in tokens and see if exception is thrown. 
 
     }
 

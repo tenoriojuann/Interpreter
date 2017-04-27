2ee10c0972a9870dd0b60fca3096573cc3bfd659
Andrew <theshatz@gmail.com>
2017-04-26 22:07:31 -0400

    commitv2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ public class Main{
 
     public static void main(String[] args) {
         try {
-            Interpreter in = new Interpreter(args[0]);
+            Interpreter in = new Interpreter("Sample.lua");
         }
         catch (Exception e) {
             System.out.println(e.getMessage());

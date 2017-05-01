7635aa11ae4b5a6463124c20b6931b63203b09b5
JUAN E TENORIO ARZOLA <tenoriojuann@gmail.com>
2017-05-01 15:26:34 -0400

    Finds file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ public class Main{
 
     public static void main(String[] args) {
         try {
-            Interpreter in = new Interpreter("Sample.lua");
+            Interpreter in = new Interpreter(Main.class.getResource("Sample.lua").getPath());
         }
         catch (Exception e) {
             System.out.println(e.getMessage());
